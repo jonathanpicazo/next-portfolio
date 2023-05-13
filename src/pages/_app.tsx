03b8036356e55b3d400eb717dbcb9b75349a8be0
Jonathan Picazo <jonathanpicazo@outlook.com>
2023-05-12 23:57:20 -0700

    changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Nav, Footer, NavBar } from "~/components";
+import { NavBar } from "~/components";
 import "dracula-ui/styles/dracula-ui.css";
 
 export default function App({ Component, pageProps }: AppProps) {
