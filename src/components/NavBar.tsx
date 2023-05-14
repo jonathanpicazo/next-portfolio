791d5e61d259b8f08c28405eb7bf0561dfe0610b
Jonathan Picazo <jonathanpicazo@outlook.com>
2023-05-13 19:43:33 -0700

    added personal info section

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,6 +8,8 @@ import { formatPathName } from "~/utils";
 import { RouterType } from "~/types";
 import { useRouter } from "next/router";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FiMenu } from "react-icons/fi";
+
 export const NavBar = () => {
   const router = useRouter();
   const [showMobileMenu, setShowMobileMenu] = useState(false);
@@ -22,24 +24,12 @@ export const NavBar = () => {
             <button
               data-collapse-toggle="navbar-default"
               type="button"
-              className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden hover:bg-dracula-dark focus:outline-none focus:ring-2 focus:ring-dracula-dark"
               aria-expanded="false"
               onClick={() => setShowMobileMenu(!showMobileMenu)}
             >
               <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
+              <FiMenu size={20} fill="#9ba0c0" />
             </button>
             {/* Desktop */}
             <div className="hidden md:block">
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -55,7 +55,7 @@ const Contact = () => {
             </div>
             <button
               type="submit"
-              className="text-dracula-darker-900 bg-dracula-purple-500 hover:bg-dracula-purple-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+              className="text-dracula-darker-900 bg-dracula-purple-500 hover:bg-dracula-purple-600 focus:ring-4 focus:outline-none focus:ring-dracula-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
             >
               Submit
             </button>
