028633b73c58bb48b3da6f242399845bc2e52878
Jonathan Picazo <jonathanpicazo@outlook.com>
2023-09-16 08:43:55 -0700

    created util function for sanity images

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
-type PostType = {
-  title: string;
-};
-export const PostCard = ({ post }: { post: any }) => {
+import { BlogPostType } from "~/types";
+import imageUrlBuilder from "@sanity/image-url";
+import Image from "next/image";
+import { client } from "~/sanityClient";
+import { urlFor } from "~/utils";
+
+export const PostCard = ({ post }: { post: BlogPostType }) => {
+  console.log("post card", post);
+  console.log("image", urlFor(post.mainImage).width(500).url());
   return (
-    <div>
-      <div>hi</div>
-    </div>
+    <article className="flex max-w-md flex-col rounded-lg border border-dracula-green-200">
+      <Image
+        alt={post.mainImage.alt}
+        src={urlFor(post.mainImage).width(500).url()}
+        width={500}
+        height={500}
+        className="aspect-video rounded-t-lg object-cover"
+      />
+      <div className="p-4">
+        <h4 className="mb-2">{post.title}</h4>
+      </div>
+    </article>
   );
 };
---
+++ src/types/sanity.d.ts
@@ -0,0 +1,14 @@
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { SanityAsset } from "@sanity/image-url/lib/types/types";
+
+export type BlogPostType = {
+  _id: string;
+  _createdAt: string;
+  title: string;
+  description: string;
+  mainImage: SanityAsset;
+  slug: {
+    current: string;
+  };
+  body: [object];
+};
