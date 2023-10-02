648c5726cf17b06dfe0928f91cd2598215cdc33c
Jonathan Picazo <jonathanpicazo@outlook.com>
2023-10-01 19:28:45 -0700

    cleaned up animations

---
+++ sanity/structures/index.ts
@@ -0,0 +1,12 @@
+// @ts-nocheck
+export const structure = (S, context) =>
+  S.list()
+    .title("Content")
+    .items([
+      S.listItem()
+        .title("Home")
+        .child(S.editor().id("home").schemaType("home").documentId("home")),
+      ...S.documentTypeListItems().filter(
+        (listItem) => !["home"].includes(listItem.getId())
+      ),
+    ]);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,23 +9,28 @@ import "easymde/dist/easymde.min.css";
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
-      <Layout>
-        <motion.div
-          key={router.route}
-          initial="pageInitial"
-          animate="pageAnimate"
-          variants={{
-            pageInitial: {
-              opacity: 0,
-            },
-            pageAnimate: {
-              opacity: 1,
-            },
-          }}
-        >
-          <Component {...pageProps} />
-        </motion.div>
-      </Layout>
+      <AnimatePresence
+        mode="wait"
+        onExitComplete={() => window.scrollTo(0, 0)}
+        initial={false}
+      >
+        <Layout>
+          <motion.div
+            key={router.route}
+            initial="hidden" // Set the initial state to variants.hidden
+            animate="enter" // Animated state to variants.enter
+            exit="exit" // Exit state (used later) to variants.exit
+            transition={{ type: "linear" }} // Set the transition to linear
+            variants={{
+              hidden: { opacity: 0, x: -100, y: 0 },
+              enter: { opacity: 1, x: 0, y: 0 },
+              exit: { opacity: 0, x: 0, y: -100 },
+            }}
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        </Layout>
+      </AnimatePresence>
     </>
   );
 }
