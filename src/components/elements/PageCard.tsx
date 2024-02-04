3a51e437c316b096abd6ca3e29ccd9c45e6071dd
Jonathan Picazo <jonathanpicazo@outlook.com>
2024-02-03 17:27:23 -0800

    feat: finished modal look and functionality

--- src/components/elements/PageCard.tsx
+++ src/components/elements/PageCard.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { ForwardedRef } from 'react';
 import { twMerge } from 'tailwind-merge';
-export const PageCard = ({
-  children,
-  className = '',
-}: {
+
+type PageCardProps = {
   children: React.ReactNode;
   className?: string;
-}) => {
-  return (
-    <section
-      className={twMerge(
-        'bg-dracula-darker-900 flex-1 rounded-lg px-3.5 pb-5 pt-4 shadow-md md:mx-2.5 md:px-9',
-        className
-      )}
-    >
-      {children}
-    </section>
-  );
 };
+
+export const PageCard = React.forwardRef<HTMLDivElement, PageCardProps>(
+  (
+    { children, className = '', ...props },
+    ref: ForwardedRef<HTMLDivElement>
+  ) => {
+    return (
+      <section
+        ref={ref}
+        className={twMerge(
+          'bg-dracula-darker-900 flex-1 rounded-lg px-3.5 pb-5 pt-4 shadow-md md:mx-2.5 md:px-9',
+          className
+        )}
+      >
+        {children}
+      </section>
+    );
+  }
+);
+
+PageCard.displayName = 'PageCard';
