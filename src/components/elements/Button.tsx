2e20108016e8aa7567fa40ab7a42cb55f6b9987e
Jonathan Picazo <jonathanpicazo@outlook.com>
2024-02-11 03:53:59 -0800

    feat: create protected route

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -1,4 +1,4 @@
-import { twMerge } from "tailwind-merge";
+import { twMerge } from 'tailwind-merge';
 
 type ButtonPropType = {
   children: React.ReactNode;
@@ -6,7 +6,7 @@ type ButtonPropType = {
 
 export const Button = ({ children, ...props }: ButtonPropType) => {
   const buttonStyles =
-    "sm:w-auto w-full rounded-lg bg-dracula-purple-500 px-5 py-2.5 text-center text-sm font-medium text-dracula-darker-900 hover:bg-dracula-purple-600 focus:outline-none focus:ring-4 focus:ring-dracula-blue-300";
+    'sm:w-auto w-full rounded-lg bg-dracula-purple-500 px-5 py-2.5 text-center text-sm font-bold text-white hover:bg-dracula-purple-600 focus:outline-none focus:ring-4 focus:ring-dracula-blue-300';
   return (
     <button className={twMerge(buttonStyles, props.className)}>
       {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    // fontSize: {
+    //   sm: ['14px', '20px'],
+    //   base: ['16px', '24px'],
+    //   lg: ['20px', '28px'],
+    //   xl: ['24px', '32px'],
+    // },
     screens: {
       md: '1024px',
       ipad: '600px',
